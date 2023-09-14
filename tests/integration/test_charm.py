3ffbf1812929d7ec0c6504ec44471596dcad92c4
Jatin Arora <jatin.arora@canonical.com>
2023-09-14 17:08:11 +0530

    feat(ci): remove check-libs and release job for github actions
    This feature disables github-action job for checking, auto-updating
    the charmhub libs, and releasing the charm. We will need to configure
    this in the future by re-enabling it when we set the `CHARMHUB_TOKEN`
    as well.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -4,9 +4,9 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-async def test_smoke(ops_test: OpsTest):
-    charm = await ops_test.build_charm(".")
-    app = await ops_test.model.deploy(charm, constraints="arch=arm64")
-    await ops_test.model.block_until(lambda: app.status in ("active", "error"), timeout=180)
-    assert app.status, "active"
 
+async def test_smoke(ops_test: OpsTest):
+    charm = await ops_test.build_charm(".", verbosity="debug")
+    app = await ops_test.model.deploy(charm)
+    await ops_test.model.block_until(lambda: app.status in ("active", "error"), timeout=300)
+    assert app.status, "active"
