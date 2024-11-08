02124a7d5cb25444bcd40ece204b0d40c1f56ef4
Jatin Arora <jatin.arora@canonical.com>
2024-11-08 13:39:09 +0530

    fix(tests): use constraints and charm if provided from the build to test

--- tests/integration/relation_tests/test_tls.py
+++ tests/integration/relation_tests/test_tls.py
@@ -1,7 +1,7 @@
 import asyncio
 
 import pytest
-from helpers import APP_NAMES, APPLICATION_APP_NAME, CHARM_NAME, TEST_APP_CHARM_PATH
+from helpers import APPLICATION_APP_NAME, TEST_APP_CHARM_PATH
 from nrpe.client import logging
 from pytest_operator.plugin import OpsTest
 
@@ -10,18 +10,28 @@ TLS_CA_CHARM_NAME = "easyrsa"
 
 @pytest.mark.skip_if_deployed
 @pytest.mark.abort_on_fail
-async def test_deploy_tls(ops_test: OpsTest):
-    charms = await ops_test.build_charms(".", TEST_APP_CHARM_PATH)
+async def test_deploy_tls(
+    ops_test: OpsTest,
+    constraints,
+    charm_path,
+    charm_name,
+):
+    if constraints:
+        await ops_test.model.set_constraints(constraints)
+    # Build and deploy charm from local source folder
+    if not charm_path:
+        charm_path = await ops_test.build_charm(".")
+    tester_charm = await ops_test.build_charm(TEST_APP_CHARM_PATH)
     async with ops_test.fast_forward():
         await asyncio.gather(
             ops_test.model.deploy(
-                charms[APPLICATION_APP_NAME],
+                tester_charm,
                 application_name=APPLICATION_APP_NAME,
                 num_units=1,
             ),
             ops_test.model.deploy(
-                charms[CHARM_NAME],
-                application_name=CHARM_NAME,
+                charm_path,
+                application_name=charm_name,
                 num_units=1,
             ),
             ops_test.model.deploy(
@@ -32,15 +42,19 @@ async def test_deploy_tls(ops_test: OpsTest):
             ),
         )
         await ops_test.model.wait_for_idle(
-            apps=[*APP_NAMES, TLS_CA_CHARM_NAME], status="active", timeout=5000
+            apps=[charm_name, APPLICATION_APP_NAME, TLS_CA_CHARM_NAME],
+            status="active",
+            timeout=5000,
         )
 
 
-async def test_tls_enabled(ops_test: OpsTest):
+async def test_tls_enabled(ops_test: OpsTest, charm_name):
     async with ops_test.fast_forward():
-        await ops_test.model.relate(f"{TLS_CA_CHARM_NAME}:client", f"{CHARM_NAME}:ca-client")
-        await ops_test.model.relate(f"{APPLICATION_APP_NAME}:client", f"{CHARM_NAME}:client")
-        await ops_test.model.wait_for_idle(apps=[*APP_NAMES, TLS_CA_CHARM_NAME], status="active")
+        await ops_test.model.relate(f"{TLS_CA_CHARM_NAME}:client", f"{charm_name}:ca-client")
+        await ops_test.model.relate(f"{APPLICATION_APP_NAME}:client", f"{charm_name}:client")
+        await ops_test.model.wait_for_idle(
+            apps=[charm_name, APPLICATION_APP_NAME, TLS_CA_CHARM_NAME], status="active"
+        )
 
 
 async def test_secrets(ops_test: OpsTest):
@@ -53,8 +67,10 @@ async def test_secrets(ops_test: OpsTest):
         pytest.skip("secrets not supported for juju < 3.0")
 
 
-async def test_adding_unit_works(ops_test: OpsTest):
+async def test_adding_unit_works(ops_test: OpsTest, charm_name):
     async with ops_test.fast_forward():
-        nats_app = ops_test.model.applications[CHARM_NAME]
+        nats_app = ops_test.model.applications[charm_name]
         await nats_app.add_units(count=2)
-        await ops_test.model.wait_for_idle(apps=[*APP_NAMES, TLS_CA_CHARM_NAME], status="active")
+        await ops_test.model.wait_for_idle(
+            apps=[charm_name, APPLICATION_APP_NAME, TLS_CA_CHARM_NAME], status="active"
+        )
--- tests/integration/relation_tests/test_upgrade.py
+++ tests/integration/relation_tests/test_upgrade.py
@@ -1,7 +1,7 @@
 import asyncio
 
 import pytest
-from helpers import APP_NAMES, APPLICATION_APP_NAME, CHARM_NAME, TEST_APP_CHARM_PATH
+from helpers import APP_NAMES, APPLICATION_APP_NAME, TEST_APP_CHARM_PATH
 from pytest_operator.plugin import OpsTest
 
 TLS_CA_CHARM_NAME = "easyrsa"
@@ -10,13 +10,15 @@ OLD_CHARM_NAME = "nats-charmers-nats"
 
 @pytest.mark.skip_if_deployed
 @pytest.mark.abort_on_fail
-async def test_deploy_old(ops_test: OpsTest):
+async def test_deploy_old(ops_test: OpsTest, constraints, charm_name):
+    if constraints:
+        await ops_test.model.set_constraints(constraints)
     test_app = await ops_test.build_charm(TEST_APP_CHARM_PATH)
     async with ops_test.fast_forward():
         await asyncio.gather(
             ops_test.model.deploy(
                 OLD_CHARM_NAME,
-                application_name=CHARM_NAME,
+                application_name=charm_name,
                 num_units=1,
                 channel="stable",
                 series="jammy",
@@ -33,8 +35,8 @@ async def test_deploy_old(ops_test: OpsTest):
                 num_units=1,
             ),
         )
-        await ops_test.model.relate(f"{TLS_CA_CHARM_NAME}:client", f"{CHARM_NAME}:ca-client")
-        await ops_test.model.relate(f"{APPLICATION_APP_NAME}:client", f"{CHARM_NAME}:client")
+        await ops_test.model.relate(f"{TLS_CA_CHARM_NAME}:client", f"{charm_name}:ca-client")
+        await ops_test.model.relate(f"{APPLICATION_APP_NAME}:client", f"{charm_name}:client")
         await ops_test.model.wait_for_idle(
             apps=[*APP_NAMES, TLS_CA_CHARM_NAME],
             status="active",
@@ -45,7 +47,13 @@ async def test_deploy_old(ops_test: OpsTest):
         )
 
 
-async def test_upgrade_switch(ops_test: OpsTest):
-    charm = await ops_test.build_charm(".")
-    await ops_test.model.applications[CHARM_NAME].refresh(path=charm)
+async def test_upgrade_switch(
+    ops_test: OpsTest,
+    charm_path,
+    charm_name,
+):
+    # Build and deploy charm from local source folder
+    if not charm_path:
+        charm_path = await ops_test.build_charm(".")
+    await ops_test.model.applications[charm_name].refresh(path=charm_path)
     await ops_test.model.wait_for_idle(apps=[*APP_NAMES, TLS_CA_CHARM_NAME], status="active")
