d6b16a96a179692bc65cc1aabb0bdb1a2445182f
gary-wzl77 <gary.wang@canonical.com>
2025-04-17 12:00:21 +0800

    fix(tests): add integration tests to relate with anbox cloud charms
    To streamline the publishing of NATS charms and prevent breaking changes
    for core consumers like Anbox Cloud charms, we now improve the NATS
    operator integration tests by relating with
    - Anbox Stream Gateway
    - Anbox Stream Agent
    charms.
    This would detect issues early before promoting the charm to the
    edge channel and facilitate a smoother charm publishing process.

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,12 +1,18 @@
 #
 # Copyright 2024 Canonical Ltd.  All rights reserved.
 #
+import asyncio
 import logging
 
 import nats
 import pytest
 from nats.errors import Error
 from pytest_operator.plugin import OpsTest
+
+STREAM_GATEWAY_NAME = "anbox-stream-gateway"
+STREAM_AGENT_NAME = "anbox-stream-agent"
+TLS_CHARM_NAME = "easyrsa"
+AMS_CHARM_NAME = "ams"
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -34,3 +40,59 @@ async def test_configured_with_token(ops_test: OpsTest, charm_name):
     with pytest.raises(Error) as e:
         await nats.connect(f"nats://{ip}:4222", allow_reconnect=False)
     assert e.value.args[0] == "nats: 'Authorization Violation'"
+
+
+async def test_relate_with_anbox_charms(
+    ops_test: OpsTest,
+    constraints,
+    charm_name,
+    charm_path,
+    anbox_cloud_version,
+):
+    await asyncio.gather(
+        ops_test.model.deploy(
+            STREAM_GATEWAY_NAME,
+            application_name=STREAM_GATEWAY_NAME,
+            num_units=1,
+            channel=f"{anbox_cloud_version}/stable",
+        ),
+        ops_test.model.deploy(
+            STREAM_AGENT_NAME,
+            application_name=STREAM_AGENT_NAME,
+            num_units=1,
+            channel=f"{anbox_cloud_version}/stable",
+        ),
+        ops_test.model.deploy(
+            AMS_CHARM_NAME,  # Required by the stream agent
+            application_name=AMS_CHARM_NAME,
+            channel=f"{anbox_cloud_version}/stable",
+            config={
+                "use_embedded_etcd": True,
+            },
+        ),
+        ops_test.model.deploy(
+            TLS_CHARM_NAME,
+            application_name=TLS_CHARM_NAME,
+            channel="latest/stable",
+            num_units=1,
+        ),
+    )
+
+    await asyncio.gather(
+        ops_test.model.relate(f"{TLS_CHARM_NAME}:client", f"{STREAM_GATEWAY_NAME}:certificates"),
+        ops_test.model.relate(f"{TLS_CHARM_NAME}:client", f"{STREAM_AGENT_NAME}:certificates"),
+        ops_test.model.relate(f"{TLS_CHARM_NAME}:client", f"{charm_name}:ca-client"),
+        ops_test.model.relate(f"{charm_name}:client", f"{STREAM_GATEWAY_NAME}:nats"),
+        ops_test.model.relate(f"{charm_name}:client", f"{STREAM_AGENT_NAME}:nats"),
+        ops_test.model.relate(f"{STREAM_AGENT_NAME}:ams", f"{AMS_CHARM_NAME}:rest-api"),
+        ops_test.model.relate(f"{STREAM_AGENT_NAME}:client", f"{AMS_CHARM_NAME}:agent"),
+    )
+
+    await ops_test.model.wait_for_idle(
+        apps=[TLS_CHARM_NAME, STREAM_GATEWAY_NAME, STREAM_AGENT_NAME, charm_name],
+        status="active",
+        timeout=2400,
+        # Do not raise on error as the charms sometime transition from
+        # an `error` state while forming relations
+        raise_on_error=False,
+    )
